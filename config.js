94cfbe08dd2abe97c34b7eb0e5a422efb865812a
mcavalon <e.abdurayimov@gmail.com>
2014-07-31 11:14:24 +0400

    modified config

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ module.exports = {
             sessionSecret: 'bxWEV7W126v9DlFJQrLSc60gqx8i3apNGDkYLKSf'
         },
         production : {
-            baseUrl      : 'http://cloudsync.eatech.org:80/',
-            port         : 80,
+            baseUrl      : 'http://localhost:3000/',
+            port         : 3000,
             cookieSecret : 'kPKFeqhemadOfq46jPg0O9FkUDH03ITLvcufHxgC',
             sessionSecret: 'VnLxoGqMzGczxW6WdeURX9DaiKAYw33sPivgBpGC'
         }
@@ -43,7 +43,7 @@ module.exports = {
                 CLIENT_ID    : '711075498703-mqr30ksshhkfifd6e71cecv8hsf2rkim.apps.googleusercontent.com',
                 CLIENT_EMAIL : '711075498703-mqr30ksshhkfifd6e71cecv8hsf2rkim@developer.gserviceaccount.com',
                 CLIENT_SECRET: 'ZIh14Ss2rRmGqalqk-KMDNlo',
-                REDIRECT_URL : 'http://cloudsync.eatech.org:80/cloud-sync/google/callback',
+                REDIRECT_URL : 'http://localhost:3000/cloud-sync/google/callback',
                 SCOPE        : 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.apps.readonly',
                 ACCESS_TYPE  : 'offline'
             },
@@ -52,7 +52,7 @@ module.exports = {
                 CLIENT_SECRET: 'i06e1fgit8jfmkx',
                 AUTH_URL     : 'https://www.dropbox.com/1/oauth/authorize',
                 ACCESS_TOKEN : 'ZAJq0ux5k_gAAAAAAAAERI1UkDeWBRSDmNg3gRQkwV96IBHFeIAZgkMXRgAqTFL9',
-                REDIRECT_URL : 'http://cloudsync.eatech.org:80/cloud-sync/dropbox/callback',
+                REDIRECT_URL : 'http://localhost:3000/cloud-sync/dropbox/callback',
                 ROOT         : 'dropbox'
             }
         }
