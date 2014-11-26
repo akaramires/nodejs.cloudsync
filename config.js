d37dbf1407403bfe0a271f9667586ba713ca1d24
mcavalon <e.abdurayimov@gmail.com>
2014-11-26 15:11:07 +0300

    some chanhes

--- config.js
+++ config.js
@@ -35,9 +35,9 @@ module.exports = {
             sessionSecret: 'VnLxoGqMzGczxW6WdeURX9DaiKAYw33sPivgBpGC',
             ga_file      : 'ga.openshift.js',
             mongoose     : {
-                server: process.env.OPENSHIFT_MONGODB_DB_HOST,
-                port  : process.env.OPENSHIFT_MONGODB_DB_PORT,
-                url   : process.env.OPENSHIFT_MONGODB_DB_URL + 'cloudsync'
+                server: 'ds051740.mongolab.com',
+                port  : 51740,
+                url   : 'mongodb://cloudsync:cloudsyncpass@ds051740.mongolab.com:51740/cloudsync'
             }
         }
     },
